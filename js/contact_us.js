369ef4a9f6f4234dd0f7e5a9a4fa8534da084e2a
Karishnu Poddar <karishnu@gmail.com>
2017-02-22 21:06:05 +0530

    fixes

--- js/contact_us.js
+++ js/contact_us.js
@@ -50,7 +50,7 @@ $(function() {
       firstName = name.split(' ').slice(0, -1).join(' ');
     }
     $.ajax({
-      url: "http://127.0.0.1:3000/auth/save",
+      url: "https://enigma2.herokuapp.com/auth/save",
       type: "POST",
       datatype: "json",
       contentType: "application/json; charset=utf-8",
@@ -67,7 +67,7 @@ $(function() {
         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
         .append("</button>");
         $('#success > .alert-success')
-        .append("<strong>" + JSON.parse(message).message + "</strong>");
+        .append("<strong>" + message.message + "</strong>");
         $('#success > .alert-success')
         .append('</div>');
       },
