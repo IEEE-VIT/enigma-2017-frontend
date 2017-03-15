c0b38d00752588012542e2ae41827d30966aeb17
Karishnu Poddar <karishnu@gmail.com>
2017-03-16 02:26:31 +0530

    ques conn

--- r/js/connections.js
+++ r/js/connections.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   console.log("Document Ready!");
   var x_access_token = Cookies.get('x-access-token');
+  var num;
   $.ajax({
       url: "https://enigma2.herokuapp.com/solve/question",
       type: "GET",
@@ -12,7 +13,30 @@ $(document).ready(function(){
       },
       success: function(message, textStatus, request) {
         $('#question_id').html(message.question);
+        num = message.num;
       }
     }
   );
+
+  $("#answerForm").submit(function(e) {
+    e.preventDefault();
+    var answer = $("input#answer_id").val();
+    $.ajax({
+      url: "https://enigma2.herokuapp.com/solve/answer",
+      type: "POST",
+      datatype: "json",
+      contentType: "application/json; charset=utf-8",
+      headers:{
+        "x-access-token":x_access_token
+      },
+      data: JSON.stringify({
+        num: num,
+        answer: answer
+      }),
+      cache: false,
+      success: function(message, textStatus, request) {
+          alert(message.message);
+      }
+    });
+  });
 });
