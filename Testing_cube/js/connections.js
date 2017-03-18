8d79c3cd1c6f2b1a95900c9607eadb3fa1f0770d
kira0204 <rshtmudgal@gmail.com>
2017-03-19 04:16:02 +0530

    Cube and UX improved

---
+++ Testing_cube/js/connections.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  console.log("Document Ready!");
+  var x_access_token = Cookies.get('x-access-token');
+  var num;
+  $.ajax({
+      url: "https://enigma2.herokuapp.com/solve/question",
+      type: "GET",
+      datatype: "json",
+      contentType: "application/json; charset=utf-8",
+      cache: false,
+      headers:{
+        "x-access-token":x_access_token
+      },
+      success: function(message, textStatus, request) {
+        $('#question_id').html(message.question);
+        num = message.num;
+      }
+    }
+  );
+
+  $('#logout_butt a').click(function(e) {
+    Cookies.remove('x-access-token');
+    window.location.replace("../index.php");
+  });
+
+  $("#answerForm").submit(function(e) {
+    e.preventDefault();
+    var answer = $("input#answer_id").val();
+    $.ajax({
+      url: "https://enigma2.herokuapp.com/solve/answer",
+      type: "POST",
+      datatype: "json",
+      contentType: "application/json; charset=utf-8",
+      headers:{
+        "x-access-token":x_access_token
+      },
+      data: JSON.stringify({
+        num: num,
+        answer: answer
+      }),
+      cache: false,
+      success: function(message, textStatus, request) {
+          window.location.replace("ques.html");
+      }
+    });
+  });
+});
---
+++ Testing_cube/js/main.js
@@ -0,0 +1,39 @@
+function main() {
+
+(
+
+
+  function () {
+   'use strict';
+          //smooth-scroll
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+    // CounterUp
+	$(document).ready(function( $ ) {
+		if($("span.count").length > 0){	
+			$('span.count').counterUp({
+					delay: 20, // the delay time in ms
+			time: 3000 // the speed time in ms
+			});
+		}
+	});
+	
+}());
+
+}
+function popup() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+main();
