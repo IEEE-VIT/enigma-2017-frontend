14515c73ef65d3c0ea51580af90a6cb978939b53
Karishnu Poddar <karishnu@gmail.com>
2017-03-31 03:07:10 +0530

    countdown fix

---
+++ r/js/connections_count.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  console.log("Document Ready!");
+  var x_access_token = Cookies.get('x-access-token');
+  var num;
+
+  $('#logout_butt a').click(function(e) {
+    Cookies.remove('x-access-token');
+    window.location.replace("../index.php");
+  });
+});
