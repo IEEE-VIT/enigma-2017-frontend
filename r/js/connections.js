5988d6a0bacf221da324cddcd7c45c3d0824c58b
Karishnu Poddar <karishnu@gmail.com>
2017-03-31 03:04:27 +0530

    countdown fix

--- r/js/connections.js
+++ r/js/connections.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   console.log("Document Ready!");
   var x_access_token = Cookies.get('x-access-token');
   var num;
+
   $.ajax({
       url: "https://enigma2.herokuapp.com/solve/question",
       type: "GET",
@@ -12,7 +13,12 @@ $(document).ready(function(){
         "x-access-token":x_access_token
       },
       success: function(message, textStatus, request) {
-        $('#ques').html("<h2 style=\"margin-top: -10px; margin-right: auto;margin-left: auto;\" id=\"question_id\">" + message.question + "</h2><img id=\"image_id\" src=\"" + message.image_url + "\" style=\"width: 20%; margin-right: auto;margin-left: auto; margin-top: 60px;\"/>");
+        if(message.type == 'default'){
+          $('#ques').html("<h2 style=\"margin-top: -10px; margin-right: auto;margin-left: auto;\" id=\"question_id\">" + message.question + "</h2><img id=\"image_id\" src=\"" + message.image_url + "\" style=\"width: 20%; margin-right: auto;margin-left: auto; margin-top: 60px;\"/>");
+        }
+        else {
+          $('#ques').html(message.data);
+        }
         $('#score_id').html(message.score);
         num = message.num;
       }
