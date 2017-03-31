9139fabd667c08b987794df6e578ec76869a3945
Karishnu Poddar <karishnu@gmail.com>
2017-04-01 02:22:05 +0530

    banned

--- admin/js/connection_admin.js
+++ admin/js/connection_admin.js
@@ -2,6 +2,27 @@ $(document).ready(function(){
 
   $("#table_body").append("<tr><td>Lol</td><td>HI</td><td>HI</td><td>HI</td></tr>");
   var x_access_token = Cookies.get('x-access-token');
+
+  $.ajax({
+      url: "https://enigma2.herokuapp.com/admin/banned",
+      type: "GET",
+      datatype: "json",
+      contentType: "application/json; charset=utf-8",
+      cache: false,
+      headers:{
+        "x-access-token":x_access_token
+      },
+      success: function(message, textStatus, request) {
+          var i = 0;
+          var string = "";
+          for(i=0; i<=message.banned; i++){
+            string = string + "<tr><td>" + (i+1)  + "</td><td>" + message.result[i].name + "</td><td>" + message.result[i].score + "</td><td>" + message.result[i].num + "</td></tr>"
+          }
+          $('#table_body').append(string);
+      }
+    }
+  );
+
   $.ajax({
       url: "https://enigma2.herokuapp.com/admin/lastquestion",
       type: "GET",
