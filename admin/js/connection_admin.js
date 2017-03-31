b471ac3f3cf657b84b39fb7251a15524cb50a0cc
Karishnu Poddar <karishnu@gmail.com>
2017-04-01 00:49:39 +0530

    admin latest solved connection

--- admin/js/connection_admin.js
+++ admin/js/connection_admin.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 
-  
   $("#table_body").append("<tr><td>Lol</td><td>HI</td><td>HI</td><td>HI</td></tr>");
-
-  $.getJSON("https://enigma2.herokuapp.com/admin/lastquestion", function(message){
-    $("#latest_ques").append(message.message[0].num);
-  });
-
+  var x_access_token = Cookies.get('x-access-token');
+  $.ajax({
+      url: "https://enigma2.herokuapp.com/admin/lastquestion",
+      type: "GET",
+      datatype: "json",
+      contentType: "application/json; charset=utf-8",
+      cache: false,
+      headers:{
+        "x-access-token":x_access_token
+      },
+      success: function(message, textStatus, request) {
+        $("#latest_ques").append(message.message[0].num);
+      }
+    }
+  );
 });
-
