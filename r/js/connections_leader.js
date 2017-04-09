2e7a765fb2ddfed04fbad020bc20b5e0c3578e07
kira0204 <rshtmudgal@gmail.com>
2017-04-10 02:02:10 +0530

    People banned

--- r/js/connections_leader.js
+++ r/js/connections_leader.js
@@ -21,7 +21,13 @@ $(document).ready(function(){
         var i = 0;
         var string = "";
         for(i=0; i<=message.result.length-1; i++){
-          string = string + "<tr><td>" + (i+1)  + "</td><td>" + message.result[i].name + "</td><td>" + ((message.result[i].college_id == null || message.result[i].college_id == 'vit_university_vellore') ? "VIT University" : message.result[i].college_id) + "</td><td>" + message.result[i].score + "</td><td>" + (message.result[i].num+1) + "</td></tr>"
+          if(message.result[i].ban_time)console.log("Test1");
+          if(message.result[i].ban){
+            string = string + "<tr style=\"background-color: rgba(187, 9, 9, 0.6);\"><td>" + (i+1)  + "</td><td>" + message.result[i].name + "</td><td>" + ((message.result[i].college_id == null || message.result[i].college_id == 'vit_university_vellore') ? "VIT University" : message.result[i].college_id) + "</td><td>" + message.result[i].score + "</td><td>" + (message.result[i].num+1) + "</td></tr>";
+          }
+          else {
+            string = string + "<tr><td>" + (i+1)  + "</td><td>" + message.result[i].name + "</td><td>" + ((message.result[i].college_id == null || message.result[i].college_id == 'vit_university_vellore') ? "VIT University" : message.result[i].college_id) + "</td><td>" + message.result[i].score + "</td><td>" + (message.result[i].num+1) + "</td></tr>";
+          }
         }
         $('#table_body').append(string);
       }
