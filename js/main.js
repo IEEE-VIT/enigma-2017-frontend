beff26e4980f018ea22d435bc9665cd61fe3e4af
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-05-21 00:41:32 +0530

    JS code shifted from html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,63 @@
+
+$(document).ready(function() {
+
+    //console.log($('#registerForm'));
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("signupp");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    var tag=0;
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+        $('.popup').hide();
+        tag=1;
+        console.log("CLICKED");
+
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+        refresh();
+    }
+
+    var f=1;
+    document.getElementById("registration").disabled = true;
+    $("#cb_is_vit").click(function(){
+
+        if(this.checked){
+            //console.log("Checked");
+            $("#txt_external_college").hide();
+            $("#uni_div").hide();
+            document.getElementById("registration").disabled = false;
+        }
+
+        else {
+            //console.log("unChecked");
+            $("#txt_external_college").show();
+            $("#uni_div").show();
+            document.getElementById("registration").disabled = true;
+        }
+    });
+
+});
+
+
+
+
+
 function main() {
 
 (
